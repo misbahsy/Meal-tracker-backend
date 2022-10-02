ffdad5806b2aa75654ccc1e05c7c290011c1ce49
Misbah Syed <misbah.sy@gmail.com>
2022-10-02 00:13:33 -0700

    server structure

--- src/server.js
+++ src/server.js
@@ -8,14 +8,14 @@ const app = express();
 app.use(bodyParser.json());
 
 routes.forEach(route => {
+    // console.log(route)
     app[route.method](route.path, route.handler);
 });
 
 const start = async () => {
     await db.connect('mongodb://localhost:27017');
-    app.listen(8080,() => {
-        console.log('Server is listening on port 8080');
-    });
+    await app.listen(8080);
+    console.log("Listening on port 8080");
 }
 
 start();
